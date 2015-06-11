a6cca5f0e8e08ef1b2df4ab8aef6bdb551787150
Nadohs <djsente@gmail.com>
2015-06-11 14:21:53 -0700

    initial commit
    protocol extension optimizied number type conversions

---
+++ numberExtensions.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import CoreGraphics
+
+ var num:Double   = 12;
+ var num2:Float   = num.f;
+ var num3:CGFloat = num.c;
+ var num4:Int     = num.i;
+ var num5:Double  = num2.d;
+
+
+
---
+++ numberExtensions.playground/Sources/NumberConvertible.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+
+import CoreGraphics //For MAC
+
+
+public protocol NumberConvertible{
+    init(_ value: Int)
+    init(_ value: Float)
+    init(_ value: Double)
+    init(_ value: CGFloat)
+    
+    
+    var c:CGFloat{ get }
+    var f:Float{ get }
+    var d:Double{ get }
+    var i:Int { get }
+}
+
+
+extension CGFloat{
+   public  init(_ value: CGFloat){
+        self.init(value)
+    }
+    
+}
+
+
+
+
+extension NumberConvertible {
+    
+    private func convert<T: NumberConvertible>(x: Self) -> T {
+        switch x {
+        case let x as CGFloat:
+            return T(x)
+        case let x as Float:
+            return T(x)
+        case let x as Int:
+            return T(x)
+        case let x as Double:
+            return T(x)
+        default:
+            return T(0)
+        }
+    }
+    
+    
+    public var c:CGFloat{
+        get{
+            return convert(self)
+        }
+        
+    }
+    public var f:Float{
+        get{
+            return convert(self)
+        }
+    }
+    public var d:Double{
+        get{
+            return convert(self)
+        }
+    }
+    public  var i:Int{
+        get{
+            return convert(self)
+        }
+    }
+}
+
+
+extension CGFloat : NumberConvertible {}
+extension Double  : NumberConvertible {}
+extension Int     : NumberConvertible {}
+extension Float   : NumberConvertible {}
+
+
+
